0fe80bbd7e927779bdade26e4e2bc943aa3a37e3
cemtatli <cemtatli3535@gmail.com>
2023-04-08 22:11:25 +0300

    tema degistirme butonu yenilendi

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -8,13 +8,12 @@ export default function ThemeChanger() {
   return (
     <button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="relative inline-flex h-5 w-10 items-center justify-center rounded-full bg-gray-200 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-gray-500 border-2 border-gray-500 dark:bg-gray-600">
-      <span className="sr-only">Switch Theme</span>
+      className="relative inline-flex h-5 w-10 items-center justify-center rounded-full bg-gray-100 ring-2 ring-white transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white dark:bg-gray-200 ">
       <span
         className={`absolute inset-y-0 left-0 flex h-5 w-5 transform items-center justify-center rounded-full bg-white shadow-md transition-all duration-300 ${
           theme === "light" ? "translate-x-5 rotate-45 " : "translate-x-0"
         }`}>
-        {theme === "dark" ? <MoonIcon width={12} className=" text-gray-600  " /> : <SunIcon width={14} className=" text-yellow-500" />}
+        {theme === "dark" ? <MoonIcon width={12} className=" text-blue-600  " /> : <SunIcon width={14} className=" text-yellow-500" />}
       </span>
     </button>
   );
