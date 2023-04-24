03ef014b42fccae7c8a446d1c1aa58ea6d575796
cemtatli <cemtatli3535@gmail.com>
2023-04-24 20:37:50 +0300

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { inject } from "@vercel/analytics";
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
