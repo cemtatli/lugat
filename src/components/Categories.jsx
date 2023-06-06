25f3e74df10f30af8c30a403189cb0f762d4e6a6
cemtatli <cemtatli3535@gmail.com>
2023-06-06 16:41:10 +0300

    update: new terms

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -16,7 +16,8 @@ const Categories = ({ children, variant = "default" }) => {
         "bg-purple-200 text-purple-800 dark:bg-purple-900 dark:text-purple-300": variant === "Web Development",
         "truncate bg-cyan-200 text-cyan-800 dark:bg-cyan-900 dark:text-cyan-300": variant === "Package Managers",
         "truncate bg-fuchsia-200 text-fuchsia-800 dark:bg-fuchsia-900 dark:text-fuchsia-300": variant === "Web Design",
-        "bg-gray-200 text-gray-800 dark:bg-gray-900 dark:text-gray-300": variant === "Next.js"
+        "bg-gray-200 text-gray-800 dark:bg-gray-900 dark:text-gray-300": variant === "Next.js",
+        "bg-rose-200 text-rose-800 dark:bg-rose-900 dark:text-rose-300": variant === "Programming Concepts"
       })}
     >
       {children}
