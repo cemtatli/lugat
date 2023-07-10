714eda61742082f4a7f81063ae8d2a42f4653873
cemtatli <cemtatli3535@gmail.com>
2023-07-10 09:30:58 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,2 +1,20 @@
-self.addEventListener("install", e => console.log("pwa installed."));
-self.addEventListener("fetch", event => {});
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("your-cache-name").then(cache => {
+      return cache.addAll([
+        "/index.html",
+        "/static/css/main.css",
+        "/static/js/main.js"
+        // ...
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
